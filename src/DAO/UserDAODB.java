2bb8d2b72fc82c6974bfaec1b15d451a482d0af1
Denzel Co <denz3550@gmail.com>
2019-04-02 00:05:39 +0800

    user dao db updates

--- src/DAO/UserDAODB.java
+++ src/DAO/UserDAODB.java
@@ -2,11 +2,8 @@ package DAO;
 
 import object.User;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
 import java.util.List;
 
 public class UserDAODB implements UserDAO{
@@ -63,7 +60,41 @@ public class UserDAODB implements UserDAO{
 
     @Override
     public void updateUser(User user) {
+        int userID = user.getUser_id();
+        String firstNameTemp = user.getFirst_name();
+        String lastNameTemp = user.getLast_name();
+        String usernameTemp = user.getUsername();
+        String passwordTemp = user.getPassword();
+        int isArtistTemp = (user.isIs_artist()) ? 1:0;
+        FileInputStream avatarStream = null;
+        try {
+            avatarStream = new FileInputStream(user.getAvatarURL());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
+        String query = "UPDATE user + SET " +
+                "user.first_name = ?, " +
+                "user.last_name = ?, " +
+                "user.username = ?, " +
+                "user.password = ?, " +
+                "user.is_artist = ?, " +
+                "user.avatar = ? WHERE user.user_id = " + userID;
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, firstNameTemp);
+            statement.setString(2, lastNameTemp);
+            statement.setString(3, usernameTemp);
+            statement.setString(4, passwordTemp);
+            statement.setInt(5, isArtistTemp);
+            statement.setBinaryStream(6, avatarStream);
+
+            statement.executeUpdate();
+            statement.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -79,5 +110,30 @@ public class UserDAODB implements UserDAO{
     @Override
     public List<User> getFollowers(User user) {
         return null;
+    }
+
+    private User toUser(ResultSet rs) throws SQLException, IOException {
+        User user = new User();
+
+        user.setUser_id(rs.getInt("user.user_id"));
+        user.setFirst_name(rs.getString("user.first_name"));
+        user.setLast_name(rs.getString("user.last_name"));
+        user.setUsername(rs.getString("user.username"));
+        user.setUsername(rs.getString("user.password"));
+        user.setIs_artist((rs.getInt("user.is_artist")!=0));
+        user.setAvatarURL(toFile(rs));
+
+        return user;
+    }
+
+    private File toFile(ResultSet rs) throws SQLException, IOException {
+        File file = new File("src/resources/" + rs.getString("user.username")+"_avatar.png");
+        OutputStream outputStream = new FileOutputStream(file);
+        InputStream inputStream = rs.getBinaryStream("user.avatar");
+        byte[] buffer = new byte[4096];
+        while (inputStream.read(buffer) > 0){
+            outputStream.write(buffer);
+        }
+        return file;
     }
 }
