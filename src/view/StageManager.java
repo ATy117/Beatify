5ffd7b4a7c594e6609f878818bea476d177666d9
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-04-01 20:51:15 +0800

    StageManager

---
+++ src/view/StageManager.java
@@ -0,0 +1,51 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StageManager {
+
+    Stage stage;
+
+    public StageManager(Stage stage){
+        this.stage = stage;
+    }
+
+    public boolean loadScene(FXMLLoader loader) {
+
+        Parent root = null;
+
+        try {
+            root = (Parent) loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+        return true;
+    }
+
+    public boolean setRightScene(BorderPane pane, FXMLLoader loader){
+        Parent root = null;
+        try{
+            root = (Parent)loader.load();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        pane.setRight(root);
+
+        return true;
+    }
+
+    public void setWindowName(String title) {
+        stage.setTitle(title);
+    }
+}
