6fdb66f1516d82c183bf3df8a5f86ca3b3e00feb
Gabriel Tan <gabtan99@gmail.com>
2019-04-02 18:23:05 +0800

    change protected

--- src/view/View.java
+++ src/view/View.java
@@ -4,13 +4,14 @@ import controller.Controller;
 import javafx.stage.Stage;
 
 public abstract class View {
-
-	public abstract void Update();
-	public Stage primaryStage;
-	public Controller controller;
-	public StageManager sm;
+	
+	protected Stage primaryStage;
+	protected Controller controller;
+	protected StageManager sm;
 
 	public View() {
 
 	}
+
+	public abstract void Update();
 }
