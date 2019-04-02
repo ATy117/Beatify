c6dc4471a2a4d9159f2c00c58ea4b63af86184c7
Denzel Co <denz3550@gmail.com>
2019-04-03 01:03:44 +0800

    add song

--- src/DAO/SongDAODB.java
+++ src/DAO/SongDAODB.java
@@ -3,7 +3,13 @@ package DAO;
 import object.Song;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class SongDAODB implements SongDAO {
@@ -11,7 +17,34 @@ public class SongDAODB implements SongDAO {
 
     @Override
     public boolean addSong(Song song) {
-        return false;
+        int songID = song.getSong_id();
+        String titleTemp = song.getSong_name();
+        String genreTemp = song.getGenre();
+        LocalDate dateUploadedTemp = song.getDate_uploaded();
+        String dateTemp = dateUploadedTemp.getYear()+"-"+dateUploadedTemp.getMonthValue()+"-"+dateUploadedTemp.getDayOfMonth();
+        int artistIDTemp = song.getArtist__id();
+        FileInputStream songFileStream = null;
+        try {
+            songFileStream = new FileInputStream(song.getSong_URL());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String query = "INSERT INTO song VALUES(NULL,?,?,?,?,?)";
+
+        try{
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            statement.setString(1, titleTemp);
+            statement.setString(2, genreTemp);
+            statement.setString(3,dateTemp);
+            statement.setInt(4, artistIDTemp);
+            statement.setBinaryStream(5, songFileStream);
+            statement.executeUpdate();
+            statement.close();
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
