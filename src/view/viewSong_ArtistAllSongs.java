fe68f1cfd69c41eba53216e5d54646e03cca286a
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-04-05 15:19:31 +0800

    control

--- src/view/viewSong_ArtistAllSongs.java
+++ src/view/viewSong_ArtistAllSongs.java
@@ -1,9 +1,28 @@
 package view;
 
+import controller.controllerSong_ArtistAllSongs;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import model.ModelCentral;
+
+import java.io.IOException;
+
 public class viewSong_ArtistAllSongs extends View {
 
-    public viewSong_ArtistAllSongs(){
+    public viewSong_ArtistAllSongs(AnchorPane mainPane, ModelCentral model, controllerSong_ArtistAllSongs controller){
+    }
 
+    public viewAllArtists (AnchorPane mainPane, ModelCentral model, controllerAllArtists controller) {
+        this.controller = controller;
+        this.model = model;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("templateAllArtists.fxml"));
+        loader.setController(this);
+
+        try {
+            mainPane.getChildren().setAll((AnchorPane) loader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
