5e3b9bad63d0714421f65a75bd78568877743fdb
Gabriel Tan <gabtan99@gmail.com>
2019-04-06 10:33:25 +0800

    commented out builder constructs

--- src/view/viewListenerDashboard.java
+++ src/view/viewListenerDashboard.java
@@ -3,6 +3,7 @@ package view;
 import com.jfoenix.controls.JFXButton;
 import controller.Controller;
 import controller.controllerDashboard;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -15,6 +16,36 @@ public class viewListenerDashboard extends viewDashboard {
 	public viewListenerDashboard (Stage primaryStage, controllerDashboard controller) {
 		super(primaryStage, controller);
 		sm.setWindowName("Beatify");
+	}
+
+	public void changePane(ActionEvent actionEvent) {
+
+		/*
+		if (actionEvent.getSource() == searchBtn) {
+			currentPaneController = new controllerSearchResults(centerCurrentAnchor, model, controller);
+		}
+		else if (actionEvent.getSource() == songsBtn) {
+			currentPaneController = new controllerAllSongs(centerCurrentAnchor, model, controller);
+		}
+		else if (actionEvent.getSource() == artistBtn) {
+			currentPaneController = new controllerAllArtists(centerCurrentAnchor, model, controller);
+		}
+		else if (actionEvent.getSource() == listenersBtn) {
+			currentPaneController = new controllerAllProfiles(centerCurrentAnchor, model, controller);
+		}
+		else if (actionEvent.getSource() == albumsBtn) {
+			currentPaneController = new controllerAllAlbums(centerCurrentAnchor, model, controller);
+		}
+		else if (actionEvent.getSource() == playlistsBtn) {
+			currentPaneController = new controllerAllPlaylists(centerCurrentAnchor, model, controller);
+		}
+		else if (actionEvent.getSource() == profileBtn) {
+			currentPaneController = new controllerMyProfile(centerCurrentAnchor, model, controller);
+		}
+
+		controller.setCurrentPane(currentPaneController);
+
+		 */
 	}
 
 	@Override
