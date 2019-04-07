2afe4cd04e0f786c68c9999d8eaf541668a00c6f
Denzel Co <denz3550@gmail.com>
2019-04-07 21:19:43 +0800

    added getFollowed artists and listeners

--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -11,7 +11,9 @@ public interface UserDAO {
 	public boolean updateUser(User user);
 	public User getUser(String username, String password);
 	public boolean checkUsername(String username);
-	public List<User> getFollowers(User user);
+	public List<User> getFollowers(int user_id);
+	public List<User> getFollowedListeners(int user_id);
+	public List<User> getFollowedArtists(int user_id);
 	public boolean followerUser(int user_id, int follower_id);
 	public boolean unfollowerUser(int user_id, int follower_id);
 	public List<User> getAllUsers(String keyword, int user_id);
