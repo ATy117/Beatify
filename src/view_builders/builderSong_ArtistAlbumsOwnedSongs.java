6b78b516e54f5068d2d87dd89b6a0d62627cf615
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-04-08 18:19:20 +0800

    fxied naming errors

--- src/view_builders/builderSong_ArtistAlbumsOwnedSongs.java
+++ src/view_builders/builderSong_ArtistAlbumsOwnedSongs.java
@@ -1,13 +1,13 @@
 package view_builders;
 
-import controller.Artist.controllerSong_ArtistAlbumsOwnedSong;
+import controller.Artist.controllerSong_ArtistAlbumsOwnedSongs;
 import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class builderSong_ArtistAlbumsOwnedSongs extends builderSong<AnchorPane> {
-    public builderSong_ArtistAlbumsOwnedSongs(controllerSong_ArtistAlbumsOwnedSong controller){
+    public builderSong_ArtistAlbumsOwnedSongs(controllerSong_ArtistAlbumsOwnedSongs controller){
         this.controller = controller;
         this.listElements = controller.getModel().getLibraryModel().getSongContents();
         this.listProducts = new ArrayList<>();
--- src/view_builders/builderSong_ArtistPlaylistFollowedSongs.java
+++ src/view_builders/builderSong_ArtistPlaylistFollowedSongs.java
@@ -1,13 +1,13 @@
 package view_builders;
 
-import controller.Artist.controllerSong_ArtistPlaylistsFollowedSongs;
+import controller.Artist.controllerSong_ArtistPlaylistFollowedSongs;
 import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class builderSong_ArtistPlaylistFollowedSongs extends builderSong<AnchorPane> {
-    public builderSong_ArtistPlaylistFollowedSongs(controllerSong_ArtistPlaylistsFollowedSongs controller){
+    public builderSong_ArtistPlaylistFollowedSongs(controllerSong_ArtistPlaylistFollowedSongs controller){
         this.controller = controller;
         this.listElements = controller.getModel().getLibraryModel().getSongContents();
         this.listProducts = new ArrayList<>();
