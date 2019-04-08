e2cc617c575928d87c204333fffc813bc2000e19
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-04-08 14:09:24 +0800

    logout

--- src/view/Listener/viewUser_ListenerMyProfile.java
+++ src/view/Listener/viewUser_ListenerMyProfile.java
@@ -1,9 +1,12 @@
 package view.Listener;
 
+import com.jfoenix.controls.JFXButton;
 import controller.controllerDashboard;
 import controller.Listener.controllerUser_ListenerMyProfile;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import view.View;
 
 import java.io.IOException;
@@ -11,6 +14,11 @@ import java.io.IOException;
 public class viewUser_ListenerMyProfile extends View {
 
     public controllerUser_ListenerMyProfile controller;
+
+    @FXML AnchorPane userContent;
+    @FXML AnchorPane userHeader;
+
+    private JFXButton logoutBtn;
 
     public viewUser_ListenerMyProfile(AnchorPane mainPane, controllerUser_ListenerMyProfile controller, controllerDashboard dashboardController){
         this.controller = controller;
@@ -23,10 +31,23 @@ public class viewUser_ListenerMyProfile extends View {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        initheader();
     }
 
     @Override
     public void Update(){
 
+    }
+
+    public void initheader(){
+        logoutBtn = new JFXButton("Logout");
+
+        logoutBtn.setFont(Font.font("Comfortaa", 12));
+
+        userHeader.setLeftAnchor(logoutBtn, 300.0);
+        userHeader.setTopAnchor(logoutBtn, 20.0);
+
+        userHeader.getChildren().add(logoutBtn);
     }
 }
