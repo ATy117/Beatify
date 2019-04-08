1a901db651ce8e790953c8bd7db0ea1fa3551237
martintierro <38676234+martintierro@users.noreply.github.com>
2019-04-08 10:41:06 +0800

    Instantiated DAOs on start rather than at each method

--- src/Facade/MasterFacade.java
+++ src/Facade/MasterFacade.java
@@ -14,8 +14,17 @@ import java.util.List;
 public class MasterFacade {
 
 	private static MasterFacade ourInstance;
+	private AlbumDAO AD;
+	private PlaylistDAO PD;
+	private SongDAO SD;
+	private UserDAO UD;
 
-	private MasterFacade(){}
+	private MasterFacade(){
+		AD = new AlbumDAODB();
+		PD = new PlaylistDAODB();
+		SD = new SongDAODB();
+		UD = new UserDAODB();
+	}
 
 	public static MasterFacade getInstance() {
 		if (ourInstance == null) {
@@ -40,7 +49,6 @@ public class MasterFacade {
 
 	// access the DAO to check if existing user, if wala pa, add a new user to DAO then return true, else return false
 	public boolean register(String username, String password, String firstName, String lastName, boolean isArtist, File profilePic) {
-		UserDAO UD = new UserDAODB();
 		if (UD.checkUsername(username)){
 			return false;
 		} else {
@@ -58,7 +66,6 @@ public class MasterFacade {
 
 	//  attempt to login using username and pass, if user is verified, return the user, else a null user will be returned
 	public User loginUser (String username, String password) {
-		UserDAO UD = new UserDAODB();
 		return UD.getUser(username, password);
 	}
 
@@ -70,12 +77,10 @@ public class MasterFacade {
 
 
 	public List<User> searchUsers(String keyword, int user_id){
-		UserDAO userDAO = new UserDAODB();
 		List<User> userList = new ArrayList<>();
-
 		//check if list retrived from dao is empty
-		if(!userDAO.getAllUsers(keyword, user_id).isEmpty()){
-			userList.addAll(userDAO.getAllUsers(keyword, user_id)); //if not empty, add all to userList
+		if(!UD.getAllUsers(keyword, user_id).isEmpty()){
+			userList.addAll(UD.getAllUsers(keyword, user_id)); //if not empty, add all to userList
 			return userList;
 		}else{
 			return userList; //returns an empty list
@@ -83,11 +88,10 @@ public class MasterFacade {
 	}
 
 	public List<Song> searchSongs(String keyword, int artist_id){
-		SongDAO songDAO = new SongDAODB();
 		List<Song> songList = new ArrayList<>();
 		//check if list retrived from dao is empty
-		if(!songDAO.getAllSongs(keyword, artist_id).isEmpty()){
-			songList.addAll(songDAO.getAllSongs(keyword, artist_id)); //if not empty, add all to songList
+		if(!SD.getAllSongs(keyword, artist_id).isEmpty()){
+			songList.addAll(SD.getAllSongs(keyword, artist_id)); //if not empty, add all to songList
 			return songList;
 		}else {
 			return songList; //returns an empty list
@@ -95,11 +99,10 @@ public class MasterFacade {
 	}
 
 	public List<Album> searchAlbums(String keyword, int artist_id){
-		AlbumDAO albumDAO = new AlbumDAODB();
 		List<Album> albumList = new ArrayList<>();
 		//check if list retrived from dao is empty
-		if(!albumDAO.getAllAlbums(keyword, artist_id).isEmpty()){
-			albumList.addAll(albumDAO.getAllAlbums(keyword, artist_id)); //if not empty, add all to albumList
+		if(!AD.getAllAlbums(keyword, artist_id).isEmpty()){
+			albumList.addAll(AD.getAllAlbums(keyword, artist_id)); //if not empty, add all to albumList
 			return albumList;
 		}else{
 			return  albumList; //returns an empty list
@@ -107,11 +110,10 @@ public class MasterFacade {
 	}
 
 	public List<Playlist> searchPlaylists(String keyword, int user_id){
-		PlaylistDAO playlistDAO = new PlaylistDAODB();
 		List<Playlist> playlistList = new ArrayList<>();
 		//check if list retrived from dao is empty
-		if(!playlistDAO.getAllPlaylists(keyword, user_id).isEmpty()){
-			playlistList.addAll(playlistDAO.getAllPlaylists(keyword, user_id)); //if not empty, add all to playlistList
+		if(!PD.getAllPlaylists(keyword, user_id).isEmpty()){
+			playlistList.addAll(PD.getAllPlaylists(keyword, user_id)); //if not empty, add all to playlistList
 			return playlistList;
 		}else{
 			return playlistList; //returns an empty list
@@ -119,34 +121,29 @@ public class MasterFacade {
 	}
 
 	// user likes song
-	public void likeSong(int userid, int songid) {
-		SongDAO songDAO = new SongDAODB();
-		songDAO.likeSong(songid, userid);
+	public boolean likeSong(int userid, int songid) {
+		return SD.likeSong(songid, userid);
 	}
 
 	// user follows album
-	public void followAlbum(int followersid, int albumid) {
-		AlbumDAO albumDAO = new AlbumDAODB();
-		albumDAO.followAlbum(albumid, followersid);
+	public boolean followAlbum(int followersid, int albumid) {
+		return AD.followAlbum(albumid, followersid);
 	}
 
 	// user follows playlist
-	public void followPlaylist(int followersid, int playlistid) {
-		PlaylistDAO playlistDAO = new PlaylistDAODB();
-		playlistDAO.followPlaylist(playlistid, followersid);
+	public boolean followPlaylist(int followersid, int playlistid) {
+		return PD.followPlaylist(playlistid, followersid);
 	}
 
 	// user follows another user
-	public void followUser(int followersid, int userid) {
-		UserDAO userDAO = new UserDAODB();
-		userDAO.followerUser(userid, followersid);
+	public boolean followUser(int followersid, int userid) {
+		return UD.followerUser(userid, followersid);
 	}
 
 	// creates a new album, accesses DAO, returns false if duplicate (?) (album name unique for the same artist or no?)
 	public boolean createAlbum (User user, Album album, File albumPic) {
-		AlbumDAO albumDAO = new AlbumDAODB();
-		if(albumDAO.checkAlbum(user.getUser_id(), album.getName())==-1) { //if checkAlbum returns -1, means there is no existing album like that
-			albumDAO.addAlbum(album);
+		if(AD.checkAlbum(user.getUser_id(), album.getName())==-1) { //if checkAlbum returns -1, means there is no existing album like that
+			AD.addAlbum(album);
 			return true; //return true if added album successfully
 		}else{
 			return false;
@@ -165,32 +162,26 @@ public class MasterFacade {
 	}
 
 	public List<Album> getMyAlbums(int user_id){
-		AlbumDAO AD = new AlbumDAODB();
 		return AD.getMyAlbums(user_id);
 	}
 
 	public List<Album> getFollowedAlbums(int user_id){
-		AlbumDAO AD = new AlbumDAODB();
 		return AD.getFollowedAlbums(user_id);
 	}
 
 	public List<Playlist> getMyPlaylists(int user_id){
-		PlaylistDAO PD = new PlaylistDAODB();
 		return PD.getMyPlaylists(user_id);
 	}
 
 	public List<Playlist> getFollowedPlaylists(int user_id){
-		PlaylistDAO PD = new PlaylistDAODB();
 		return PD.getFollowedPlaylists(user_id);
 	}
 
 	public List<Song> getMySongs(int user_id){
-		SongDAO SD = new SongDAODB();
 		return SD.getOwnerSongs(user_id);
 	}
 
 	public List<Song> getLikedSongs(int user_id){
-		SongDAO SD = new SongDAODB();
 		return SD.getLikedSongs(user_id);
 	}
 
