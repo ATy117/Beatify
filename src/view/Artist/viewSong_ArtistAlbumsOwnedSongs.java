6bf94e4f3c0c424da823c5ccaf1c72f181265be0
Jarrett Ethan Singian <jarsingian@gmail.com>
2019-04-08 19:47:16 +0800

    viewSong_Artist has been filled.

--- src/view/Artist/viewSong_ArtistAlbumsOwnedSongs.java
+++ src/view/Artist/viewSong_ArtistAlbumsOwnedSongs.java
@@ -1,17 +1,25 @@
 package view.Artist;
 
+import com.jfoenix.controls.JFXListView;
 import controller.Artist.controllerSong_ArtistAlbumsOwnedSong;
 import controller.Artist.controllerSong_ArtistAllSongs;
 import controller.controllerDashboard;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import view.View;
 
 import java.io.IOException;
 
+//Songs inside MY Album
 public class viewSong_ArtistAlbumsOwnedSongs extends View {
-
-    //Songs inside MY Album
+    @FXML AnchorPane songsHeader;
+    @FXML JFXListView songListView;
+    private Label headerLabel;
+    private Label subheaderLabel;
     private controllerSong_ArtistAlbumsOwnedSong controller;
 
     public viewSong_ArtistAlbumsOwnedSongs(AnchorPane mainPane, controllerSong_ArtistAlbumsOwnedSong controller, controllerDashboard dashboardController)
@@ -26,10 +34,66 @@ public class viewSong_ArtistAlbumsOwnedSongs extends View {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        initHeader();
+        Update();
+
     }
 
     @Override
-    public void Update() {
-
+    public void Update(){
+        setSongs();
     }
+
+    private void initHeader () {
+        //INITIALIZES THE HEADER//
+        String ALBUMNAME = "47: INSERT ARTIST ALBUM";
+        String ARTISTNAME = "Album by me";
+        headerLabel = new Label(ALBUMNAME);
+        subheaderLabel = new Label(ARTISTNAME);
+        headerLabel.setFont(Font.font("Comfortaa", 18));
+        subheaderLabel.setFont(Font.font("Comfortaa",12));
+        songsHeader.setLeftAnchor(headerLabel, 285.0);
+        songsHeader.setTopAnchor(headerLabel, 50.0);
+        songsHeader.setLeftAnchor(subheaderLabel, 285.0);
+        songsHeader.setTopAnchor(subheaderLabel, 70.0);
+        songsHeader.getChildren().add(headerLabel);
+        songsHeader.getChildren().add(subheaderLabel);
+    }
+    private void setSongs () {
+        songListView.getItems().clear();
+        //SETS SONGS//
+        for (int i = 0; i < 5; i++) {
+            //place holder values//
+            AnchorPane songsIndiv = new AnchorPane();
+            Text titleText = new Text("Covered in Roses");
+       //     Text artistText = new Text("Dr Jekyl");
+            Text albumText = new Text("In Response to Bad Events");
+            Text yearText = new Text("2019");
+            Text genreText = new Text("Hip Hop");
+
+            songsIndiv.setTopAnchor(titleText, 0.0);
+          //  songsIndiv.setTopAnchor(artistText, 18.0);
+            songsIndiv.setTopAnchor(albumText, 0.0);
+            songsIndiv.setTopAnchor(yearText, 0.0);
+            songsIndiv.setTopAnchor(genreText, 18.0);
+
+            songsIndiv.setLeftAnchor(titleText, 50.0);
+          //  songsIndiv.setLeftAnchor(artistText, 50.0);
+            songsIndiv.setLeftAnchor(albumText, 300.0);
+            songsIndiv.setLeftAnchor(yearText, 500.0);
+            songsIndiv.setLeftAnchor(genreText, 500.0);
+
+            songsIndiv.getChildren().add(titleText);
+          //  songsIndiv.getChildren().add(artistText);
+            songsIndiv.getChildren().add(albumText);
+            songsIndiv.getChildren().add(yearText);
+            songsIndiv.getChildren().add(genreText);
+
+            songListView.getItems().add(songsIndiv);
+
+        }
+    }
+
+
+
 }
