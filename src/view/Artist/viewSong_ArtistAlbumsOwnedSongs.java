79f0608f4a184fe934c436604b3b2eea0cfd4dbb
Jarrett Ethan Singian <jarsingian@gmail.com>
2019-04-09 18:13:38 +0800

    Sorting has been applied to ArtistAlbum, ArtistSong and  ArtistPlaylist.

--- src/view/Artist/viewSong_ArtistAlbumsOwnedSongs.java
+++ src/view/Artist/viewSong_ArtistAlbumsOwnedSongs.java
@@ -1,5 +1,6 @@
 package view.Artist;
 
+import com.jfoenix.controls.JFXComboBox;
 import controller.Artist.controllerSong_ArtistAlbumsOwnedSongs;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
@@ -21,6 +22,8 @@ import view_builders.builderSong;
 import view_builders.builderSong_ArtistAlbumsOwnedSongs;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 //Songs inside MY Album
 public class viewSong_ArtistAlbumsOwnedSongs extends View {
@@ -29,8 +32,11 @@ public class viewSong_ArtistAlbumsOwnedSongs extends View {
     private Label headerLabel;
     private Label subheaderLabel;
     private controllerSong_ArtistAlbumsOwnedSongs controller;
+    private Label sortLabel;
+    JFXComboBox sortingCB;
+    JFXComboBox arrangeCB;
     private Album selectedAlbum;
-
+    ArrayList<AnchorPane> sortArrayList; //used to tempoarily store the anchorpane for sorting
     //Songs inside MY Album
 
     public viewSong_ArtistAlbumsOwnedSongs(AnchorPane mainPane, controllerSong_ArtistAlbumsOwnedSongs controller, controllerDashboard dashboardController)
@@ -46,33 +52,39 @@ public class viewSong_ArtistAlbumsOwnedSongs extends View {
             e.printStackTrace();
         }
         selectedAlbum = model.getLibraryModel().getSelectedAlbum();
+        setSongs();
         initHeader();
+        initSortComboBox();
         Update();
 
     }
 
     @Override
-    public void Update(){
+    public void Update() {
         setSongs();
+        sortSongListView();
     }
-
     private void initHeader () {
         //INITIALIZES THE HEADER//
         String ALBUMNAME = selectedAlbum.getName();
         String ARTISTNAME = "Album by You";
         headerLabel = new Label(ALBUMNAME);
         subheaderLabel = new Label(ARTISTNAME);
+        sortLabel = new Label("Sort by");
         JFXButton uploadSongBtn = new JFXButton("+ upload song");
         headerLabel.setFont(Font.font("Comfortaa", 18));
         subheaderLabel.setFont(Font.font("Comfortaa",12));
-        songsHeader.setLeftAnchor(headerLabel, 285.0);
+        sortLabel.setFont(Font.font("Comfortaa", 18));
+        songsHeader.setLeftAnchor(headerLabel, 50.0);
         songsHeader.setTopAnchor(headerLabel, 50.0);
-        songsHeader.setLeftAnchor(subheaderLabel, 285.0);
+        songsHeader.setLeftAnchor(subheaderLabel, 50.0);
         songsHeader.setTopAnchor(subheaderLabel, 70.0);
-        songsHeader.setLeftAnchor(uploadSongBtn, 400.0);
-        songsHeader.setTopAnchor(uploadSongBtn, 70.0);
+        songsHeader.setLeftAnchor(sortLabel, 475.0);
+        songsHeader.setTopAnchor(sortLabel, 28.0);
+
         songsHeader.getChildren().add(headerLabel);
         songsHeader.getChildren().add(subheaderLabel);
+        songsHeader.getChildren().add(sortLabel);
         songsHeader.getChildren().add(uploadSongBtn);
 
         uploadSongBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -84,15 +96,217 @@ public class viewSong_ArtistAlbumsOwnedSongs extends View {
     }
     private void setSongs () {
         songListView.getItems().clear();
-
+        sortArrayList = new ArrayList<>();
         builderSong builder = new builderSong_ArtistAlbumsOwnedSongs(controller);
         Director director = Director.getInstance();
         director.setBuilder(builder);
         director.construct();
 
-        for (Object object: builder.getProduct()){
+        for (Object object : builder.getProduct()) {
             AnchorPane anchorPane = (AnchorPane) object;
-            songListView.getItems().add(anchorPane);
+            sortArrayList.add(anchorPane);
+        }
+    }
+
+    private void initSortComboBox() {
+        //CREATES SORTING AND ARRANGEMENT //
+        sortingCB = new JFXComboBox();
+        arrangeCB = new JFXComboBox();
+        String[] categories = new String[]{"Title", "Artist", "Album", "Year", "Genre", "Date Added"};
+        String[] arrangement = new String[]{"Ascending", "Descending"};
+
+        for (String e : categories)
+            sortingCB.getItems().add(e);
+
+        for (String e : arrangement)
+            arrangeCB.getItems().add(e);
+
+        songsHeader.setLeftAnchor(sortingCB, 375.0);
+        songsHeader.setTopAnchor(sortingCB, 50.0);
+        songsHeader.setLeftAnchor(arrangeCB, 500.0);
+        songsHeader.setTopAnchor(arrangeCB, 50.0);
+        songsHeader.getChildren().add(sortingCB);
+        songsHeader.getChildren().add(arrangeCB);
+
+        sortingCB.setOnAction(event -> {
+            sortSongListView();
+        });
+
+        arrangeCB.setOnAction(event -> {
+            sortSongListView();
+        });
+
+        sortingCB.setValue("Date Added");
+        arrangeCB.setValue("Descending");
+        sortByDateAdded();
+
+    }
+    private void sortSongListView (){
+        String choice = (String) sortingCB.getValue();
+        switch (choice){
+            case "Title":
+                sortByTitle();
+                break;
+            case "Album":
+                sortByAlbum();
+                break;
+            case "Year":
+                sortByYear();
+                break;
+            case "Genre":
+                sortByGenre();
+                break;
+            case "Date Added":
+                sortByDateAdded();
+                break;
+            default:
+                System.out.println("SORTING ERROR");
+
+        }
+
+    }
+
+
+
+    private void sortByTitle() {
+        songListView.getItems().clear();
+        String arrange = (String) arrangeCB.getValue();
+        {
+
+            for (int i = 0; i < sortArrayList.size() - 1; i++) {
+                for (int j = 0; j < sortArrayList.size() - i - 1; j++) {
+
+                    Text t1 = (Text) sortArrayList.get(j).getChildren().get(0);
+                    Text t2 = (Text) sortArrayList.get(j + 1).getChildren().get(0);
+                    String s1 = t1.getText().toLowerCase();
+                    String s2 = t2.getText().toLowerCase();
+                    if (arrange.compareTo("Ascending") == 0){
+                        if (s1.compareTo(s2) > 0) {
+                            Collections.swap(sortArrayList, j, j + 1);
+                        }
+                    }
+                    else{
+                        if (s1.compareTo(s2) < 0) {
+                            Collections.swap(sortArrayList, j, j + 1);
+                        }
+                    }
+
+
+                }
+            }
+            for (AnchorPane e : sortArrayList) {
+                System.out.println("Title Added Ascending");
+                songListView.getItems().add(e);
+
+            }
+        }
+
+    }
+    private void sortByAlbum(){
+        songListView.getItems().clear();
+        String arrange = (String) arrangeCB.getValue();
+        {
+
+            for (int i = 0; i < sortArrayList.size() - 1; i++) {
+                for (int j = 0; j < sortArrayList.size() - i - 1; j++) {
+
+                    Text t1 = (Text) sortArrayList.get(j).getChildren().get(1);
+                    Text t2 = (Text) sortArrayList.get(j + 1).getChildren().get(1);
+                    String s1 = t1.getText().toLowerCase();
+                    String s2 = t2.getText().toLowerCase();
+                    if (arrange.compareTo("Ascending") == 0){
+                        if (s1.compareTo(s2) > 0) {
+                            Collections.swap(sortArrayList, j, j + 1);
+                        }
+                    }
+                    else{
+                        if (s1.compareTo(s2) < 0) {
+                            Collections.swap(sortArrayList, j, j + 1);
+                        }
+                    }
+                }
+            }
+            for (AnchorPane e : sortArrayList) {
+                System.out.println("Title Added Ascending");
+                songListView.getItems().add(e);
+
+            }
+        }
+    }
+    private void sortByYear () {
+        songListView.getItems().clear();
+        String arrange = (String) arrangeCB.getValue();
+
+
+        for (int i = 0; i < sortArrayList.size() - 1; i++) {
+            for (int j = 0; j < sortArrayList.size() - i - 1; j++) {
+
+                Text t1 = (Text) sortArrayList.get(j).getChildren().get(2);
+                Text t2 = (Text) sortArrayList.get(j + 1).getChildren().get(2);
+                int s1 = Integer.parseInt(t1.getText());
+                int s2 = Integer.parseInt(t2.getText());
+                if (arrange.compareTo("Ascending") == 0) {
+                    if (s1 > s2) {
+                        Collections.swap(sortArrayList, j, j + 1);
+                    }
+                } else {
+                    if (s1 < s2) {
+                        Collections.swap(sortArrayList, j, j + 1);
+                    }
+                }
+            }
+        }
+        for (AnchorPane e : sortArrayList) {
+            System.out.println("Title Added Ascending");
+            songListView.getItems().add(e);
+
+        }
+    }
+    private void sortByGenre(){
+        songListView.getItems().clear();
+        String arrange = (String) arrangeCB.getValue();
+
+
+        for (int i = 0; i < sortArrayList.size() - 1; i++) {
+            for (int j = 0; j < sortArrayList.size() - i - 1; j++) {
+
+                Text t1 = (Text) sortArrayList.get(j).getChildren().get(3);
+                Text t2 = (Text) sortArrayList.get(j + 1).getChildren().get(3);
+                String s1 = t1.getText().toLowerCase();
+                String s2 = t2.getText().toLowerCase();
+                if (arrange.compareTo("Ascending") == 0) {
+                    if (s1.compareTo(s2) > 0) {
+                        Collections.swap(sortArrayList, j, j + 1);
+                    }
+                } else {
+                    if (s1.compareTo(s2) < 0) {
+                        Collections.swap(sortArrayList, j, j + 1);
+                    }
+                }
+            }
+        }
+        for (AnchorPane e : sortArrayList) {
+            System.out.println("Title Added Ascending");
+            songListView.getItems().add(e);
+        }
+
+    }
+    private void sortByDateAdded() {
+        songListView.getItems().clear();
+        String arrange = (String) arrangeCB.getValue();
+        if (arrange.compareTo("Ascending") == 0) {
+            for (int i = 0; i < sortArrayList.size() - 1; i++){
+                for (int j = 0; j < sortArrayList.size() - i - 1; j++){
+                    Collections.swap(sortArrayList, j, j + 1);
+                }
+            }
+
+        } else {
+            setSongs();
+        }
+        for (AnchorPane e : sortArrayList) {
+            System.out.println("Date Added Descending");
+            songListView.getItems().add(e);
         }
     }
 
