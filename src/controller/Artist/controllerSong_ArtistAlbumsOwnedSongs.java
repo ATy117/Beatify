9ea6f93fd3d76461d476b8febd6066920b8e435f
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-04-09 14:28:48 +0800

    can delete songs from album

--- src/controller/Artist/controllerSong_ArtistAlbumsOwnedSongs.java
+++ src/controller/Artist/controllerSong_ArtistAlbumsOwnedSongs.java
@@ -4,6 +4,7 @@ import controller.PaneController;
 import controller.controllerDashboard;
 import controller.controller_AddSong;
 import javafx.scene.layout.AnchorPane;
+import object.Song;
 import view.Artist.viewSearchables_ArtistAllSearchResults;
 import view.Artist.viewSong_ArtistAlbumsOwnedSongs;
 import view.Artist.viewSong_ArtistPlaylistsOwnedSongs;
@@ -14,6 +15,14 @@ public class controllerSong_ArtistAlbumsOwnedSongs extends PaneController  {
 		super(dashboardController);
 		this.model = dashboardController.getModel();
 		view = new viewSong_ArtistAlbumsOwnedSongs(mainPane,this, dashboardController);
+	}
+
+	public void deleteSong(Song song){
+		facade.deleteSong(song.getSong_id());
+		model.getLibraryModel().setSelectedAlbum(facade.getAlbum(song.getAlbum_id()));
+		model.getLibraryModel().setSongContents(facade.getAlbumSongs(song.getAlbum_id()));
+		controllerSong_ArtistAlbumsOwnedSongs c = new controllerSong_ArtistAlbumsOwnedSongs(dashboardController.getPaneFoundation(), dashboardController);
+		dashboardController.setCurrentPane(this);
 	}
 
 	public void uploadSong() {
