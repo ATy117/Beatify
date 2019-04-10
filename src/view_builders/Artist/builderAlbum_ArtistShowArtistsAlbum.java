0b803ac8a40d2fcb0bfeddf481635a506421bd1f
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-04-10 10:57:42 +0800

    buildersListeners

---
+++ src/view_builders/Artist/builderAlbum_ArtistShowArtistsAlbum.java
@@ -0,0 +1,66 @@
+package view_builders.Artist;
+
+import com.jfoenix.controls.JFXPopup;
+import controller.Artist.controllerUser_ArtistShowArtistsProfile;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.TextAlignment;
+import object.Album;
+import view_builders.builderAlbum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class builderAlbum_ArtistShowArtistsAlbum extends builderAlbum<AnchorPane> {
+
+    private controllerUser_ArtistShowArtistsProfile controller;
+
+    public builderAlbum_ArtistShowArtistsAlbum(controllerUser_ArtistShowArtistsProfile controller) {
+        this.controller = controller;
+        this.listElements = controller.getModel().getPeopleModel().getAlbums();
+        this.listProducts = new ArrayList<>();
+    }
+
+    @Override
+    public void build() {
+        while(listElements.hasNext()) {
+            Album album = listElements.next();
+            AnchorPane albumIndiv = new AnchorPane();
+            Circle albumCover = new Circle(45);
+            Label text = new Label(album.getName());
+
+            albumIndiv.setLeftAnchor(albumCover, 20.0);
+            albumIndiv.setTopAnchor(albumCover, 13.0);
+            albumIndiv.setTopAnchor(text, 106.0);
+            albumIndiv.setLeftAnchor(text, 47.0);
+
+            albumIndiv.getChildren().add(albumCover);
+            albumIndiv.getChildren().add(text);
+
+            text.setMaxWidth(130.0);
+            text.setAlignment(Pos.CENTER);
+            text.setWrapText(true);
+            text.setTextAlignment(TextAlignment.CENTER);
+
+            albumCover.setFill(new ImagePattern(new Image(album.getCover_URL().toURI().toString())));
+
+            listProducts.add(albumIndiv);
+        }
+
+    }
+
+    @Override
+    public List<AnchorPane> getProduct() {
+        return listProducts;
+    }
+}
+
---
+++ src/view_builders/Artist/builderPlaylist_ArtistPlaylistFollowed.java
@@ -0,0 +1,89 @@
+package view_builders.Artist;
+
+import com.jfoenix.controls.JFXPopup;
+import controller.Artist.controllerPlaylist_ArtistsAllPlaylists;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import object.Playlist;
+import view_builders.builderPlaylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class builderPlaylist_ArtistPlaylistFollowed  extends builderPlaylist<AnchorPane> {
+
+    private controllerPlaylist_ArtistsAllPlaylists controller;
+
+    public builderPlaylist_ArtistPlaylistFollowed(controllerPlaylist_ArtistsAllPlaylists controller) {
+        this.controller = controller;
+        this.listElements = controller.getModel().getLibraryModel().getFollowedPlaylists();
+        this.listProducts = new ArrayList<>();
+    }
+
+    @Override
+    public void build() {
+        while(listElements.hasNext()) {
+            Playlist playlist = listElements.next();
+            AnchorPane albumIndiv = new AnchorPane();
+            Circle albumCover = new Circle(45);
+            Label text = new Label(playlist.getName());
+
+            Image publicImg = new Image("resources/publicCover.png");
+            albumCover.setFill(new ImagePattern(publicImg));
+
+            JFXPopup popup = new JFXPopup();
+            VBox content = new VBox();
+            content.setPrefWidth(65);
+            Button unfollowButton = new Button("Unfollow");
+            unfollowButton.setMinWidth(content.getPrefWidth());
+            content.getChildren().addAll(unfollowButton);
+            popup.setPopupContent(content);
+
+            albumIndiv.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if (event.getEventType().equals(MouseEvent.MOUSE_CLICKED)) {
+                        if (((MouseEvent) event).getButton().equals(MouseButton.SECONDARY)) {
+                            popup.show(albumIndiv, JFXPopup.PopupVPosition.TOP, JFXPopup.PopupHPosition.RIGHT);
+                        } else {
+                            controller.goToFollowedList(playlist.getPlaylist_id());
+                        }
+                    }
+                }
+            });
+
+            unfollowButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    controller.unfollowPlaylist(playlist.getPlaylist_id());
+                    popup.hide();
+                }
+            });
+
+            albumIndiv.setLeftAnchor(albumCover, 20.0);
+            albumIndiv.setTopAnchor(albumCover, 13.0);
+            albumIndiv.setTopAnchor(text, 102.0);
+            albumIndiv.setLeftAnchor(text, 24.0);
+
+            albumIndiv.getChildren().add(albumCover);
+            albumIndiv.getChildren().add(text);
+            listProducts.add(albumIndiv);
+        }
+
+    }
+
+    @Override
+    public List<AnchorPane> getProduct() {
+        return listProducts;
+    }
+}
