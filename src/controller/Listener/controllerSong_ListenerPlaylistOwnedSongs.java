88db853f2fc91bd41ff530f90fd3cfc3765125c9
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-04-10 12:29:11 +0800

    constructors

--- src/controller/Listener/controllerSong_ListenerPlaylistOwnedSongs.java
+++ src/controller/Listener/controllerSong_ListenerPlaylistOwnedSongs.java
@@ -1,4 +1,16 @@
 package controller.Listener;
 
-public class controllerSong_ListenerPlaylistOwnedSongs {
+import controller.PaneController;
+import controller.controllerDashboard;
+import javafx.scene.layout.AnchorPane;
+import view.Artist.viewSong_ArtistPlaylistsOwnedSongs;
+import view.Listener.viewSong_ListenerPlaylistOwnedSongs;
+
+public class controllerSong_ListenerPlaylistOwnedSongs extends PaneController {
+
+    public controllerSong_ListenerPlaylistOwnedSongs(AnchorPane mainPane, controllerDashboard dashboardController){
+        super(mainPane, dashboardController);
+        this.model = dashboardController.getModel();
+        view = new viewSong_ListenerPlaylistOwnedSongs(  mainPane,this, dashboardController);
+    }
 }
