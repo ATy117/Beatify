751079d186a9e0d73b8a58c2a85f702ab75b7ef1
Gabriel Tan <gabtan99@gmail.com>
2019-04-11 10:51:21 +0800

    just moved a line to listner

--- src/controller/Listener/controllerListenerDashboard.java
+++ src/controller/Listener/controllerListenerDashboard.java
@@ -10,6 +10,7 @@ public class controllerListenerDashboard extends controllerDashboard {
 	public controllerListenerDashboard (Stage primaryStage, User user) {
 		super(primaryStage, user);
 		model.getProfileModel().setUser(user);
+		model.getLibraryModel().setMyPlaylists(facade.getMyPlaylists(user.getUser_id()));
 		model.getProfileModel().setFollowedListeners(facade.getFollowedListeners(this.model.getProfileModel().getUser().getUser_id()));
 		view = new viewListenerDashboard(primaryStage, this);
 		model.AttachToAll(view);
