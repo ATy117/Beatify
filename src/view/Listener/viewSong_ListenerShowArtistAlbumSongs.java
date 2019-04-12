d340d9f3431f3e07210fb6da29f34e17f0a6a623
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-04-12 09:14:46 +0800

    Combo

--- src/view/Listener/viewSong_ListenerShowArtistAlbumSongs.java
+++ src/view/Listener/viewSong_ListenerShowArtistAlbumSongs.java
@@ -115,6 +115,9 @@ public class viewSong_ListenerShowArtistAlbumSongs extends View {
         String[] categories = new String[]{"Title", "Artist", "Album", "Year", "Genre", "Date Added"};
         String[] arrangement = new String[]{"Ascending", "Descending"};
 
+        sortingCB.setId("comboStyle");
+        arrangeCB.setId("comboStyle");
+
         for (String e : categories)
             sortingCB.getItems().add(e);
 
